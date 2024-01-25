c5af563843fe9f4a4fbf1e7ba091412c658744a0
Xiaoyao Li <xiaoyao.li@intel.com>
2024-01-24 21:40:16 -0500

    i386/cpuid: Move leaf 7 to correct group
    commit 0729857c707535847d7fe31d3d91eb8b2a118e3c upstream.
    
    CPUID leaf 7 was grouped together with SGX leaf 0x12 by commit
    b9edbadefb9e ("i386: Propagate SGX CPUID sub-leafs to KVM") by mistake.
    
    SGX leaf 0x12 has its specific logic to check if subleaf (starting from 2)
    is valid or not by checking the bit 0:3 of corresponding EAX is 1 or
    not.
    
    Leaf 7 follows the logic that EAX of subleaf 0 enumerates the maximum
    valid subleaf.
    
    Intel-SIG: commit 0729857c7075 i386/cpuid: Move leaf 7 to correct group
    Backport i386/cpu fixes
    
    Fixes: b9edbadefb9e ("i386: Propagate SGX CPUID sub-leafs to KVM")
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Message-ID: <20240125024016.2521244-4-xiaoyao.li@intel.com>
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- target/i386/kvm/kvm.c
+++ target/i386/kvm/kvm.c
@@ -2001,7 +2001,6 @@ int kvm_arch_init_vcpu(CPUState *cs)
                 c = &cpuid_data.entries[cpuid_i++];
             }
             break;
-        case 0x7:
         case 0x12:
             for (j = 0; ; j++) {
                 c->function = i;
@@ -2021,6 +2020,7 @@ int kvm_arch_init_vcpu(CPUState *cs)
                 c = &cpuid_data.entries[cpuid_i++];
             }
             break;
+        case 0x7:
         case 0x14:
         case 0x1d:
         case 0x1e: {
