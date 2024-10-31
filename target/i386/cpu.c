b116c4ac3295d3638079ae4a778b18f51c88f554
Tao Su <tao1.su@linux.intel.com>
2024-10-31 16:52:31 +0800

    target/i386: Add feature dependencies for AVX10
    commit 150ab84b2d0083e6af344cca70290614d4fe568d upstream.
    
    Since the highest supported vector length for a processor implies that
    all lesser vector lengths are also supported, add the dependencies of
    the supported vector lengths. If all vector lengths aren't supported,
    clear AVX10 enable bit as well.
    
    Note that the order of AVX10 related dependencies should be kept as:
            CPUID_24_0_EBX_AVX10_128     -> CPUID_24_0_EBX_AVX10_256,
            CPUID_24_0_EBX_AVX10_256     -> CPUID_24_0_EBX_AVX10_512,
            CPUID_24_0_EBX_AVX10_VL_MASK -> CPUID_7_1_EDX_AVX10,
            CPUID_7_1_EDX_AVX10          -> CPUID_24_0_EBX,
    so that prevent user from setting weird CPUID combinations, e.g. 256-bits
    and 512-bits are supported but 128-bits is not, no vector lengths are
    supported but AVX10 enable bit is still set.
    
    Since AVX10_128 will be reserved as 1, adding these dependencies has the
    bonus that when user sets -cpu host,-avx10-128, CPUID_7_1_EDX_AVX10 and
    CPUID_24_0_EBX will be disabled automatically.
    
    Intel-SIG: commit 150ab84b2d00 target/i386: Add feature dependencies for AVX10.
    GNR AVX10.1 backporting
    
    Tested-by: Xuelian Guo <xuelian.guo@intel.com>
    Signed-off-by: Tao Su <tao1.su@linux.intel.com>
    Link: https://lore.kernel.org/r/20241028024512.156724-5-tao1.su@linux.intel.com
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Link: https://lore.kernel.org/r/20241031085233.425388-7-tao1.su@linux.intel.com
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -1565,6 +1565,22 @@ static FeatureDep feature_dependencies[] = {
         .from = { FEAT_8000_0001_ECX,       CPUID_EXT3_SVM },
         .to = { FEAT_SVM,                   ~0ull },
     },
+    {
+        .from = { FEAT_24_0_EBX,            CPUID_24_0_EBX_AVX10_128 },
+        .to = { FEAT_24_0_EBX,              CPUID_24_0_EBX_AVX10_256 },
+    },
+    {
+        .from = { FEAT_24_0_EBX,            CPUID_24_0_EBX_AVX10_256 },
+        .to = { FEAT_24_0_EBX,              CPUID_24_0_EBX_AVX10_512 },
+    },
+    {
+        .from = { FEAT_24_0_EBX,            CPUID_24_0_EBX_AVX10_VL_MASK },
+        .to = { FEAT_7_1_EDX,               CPUID_7_1_EDX_AVX10 },
+    },
+    {
+        .from = { FEAT_7_1_EDX,             CPUID_7_1_EDX_AVX10 },
+        .to = { FEAT_24_0_EBX,              ~0ull },
+    },
 };
 
 typedef struct X86RegisterInfo32 {
