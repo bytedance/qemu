6935372ab389d1fe16a515a15e290a7cf3730d8b
Zhao Liu <zhao1.liu@intel.com>
2025-07-11 18:21:38 +0800

    i386/cpu: Select legacy cache model based on vendor in CPUID 0x2
    commit 2f5b76bce5c9aa71b35bbe5fb666343eed7478f1 upstream.
    
    As preparation for merging cache_info_cpuid4 and cache_info_amd in
    X86CPUState, set legacy cache model based on vendor in the CPUID 0x2
    leaf. For AMD CPU, select legacy AMD cache model (in cache_info_amd) as
    the default cache model, otherwise, select legacy Intel cache model (in
    cache_info_cpuid4) as before.
    
    To ensure compatibility is not broken, add an enable_legacy_vendor_cache
    flag based on x-vendor-only-v2 to indicate cases where the legacy cache
    model should be used regardless of the vendor. For CPUID 0x2 leaf,
    enable_legacy_vendor_cache flag indicates to pick legacy Intel cache
    model, which is for compatibility with the behavior of PC machine v10.0
    and older.
    
    The following explains how current vendor-based default legacy cache
    model ensures correctness without breaking compatibility.
    
    * For the PC machine v6.0 and older, vendor_cpuid_only=false, and
      vendor_cpuid_only_v2=false.
    
      - If the named CPU model has its own cache model, and doesn't use
        legacy cache model (legacy_cache=false), then cache_info_cpuid4 and
        cache_info_amd are same, so 0x2 leaf uses its own cache model
        regardless of the vendor.
    
      - For max/host/named CPU (without its own cache model), then the flag
        enable_legacy_vendor_cache is true, they will use legacy Intel cache
        model just like their previous behavior.
    
    * For the PC machine v10.0 and older (to v6.1), vendor_cpuid_only=true,
      and vendor_cpuid_only_v2=false.
    
      - If the named CPU model has its own cache model (legacy_cache=false),
        then cache_info_cpuid4 & cache_info_amd both equal to its own cache
        model, so it uses its own cache model in 0x2 leaf regardless of the
        vendor. Only AMD CPUs have all-0 leaf due to vendor_cpuid_only=true,
        and this is exactly the behavior of these old machines.
    
      - For max/host/named CPU (without its own cache model), then the flag
        enable_legacy_vendor_cache is true, they will use legacy Intel cache
        model. Similarly, only AMD CPUs have all-0 leaf, and this is exactly
        the behavior of these old machines.
    
    * For the PC machine v10.1 and newer, vendor_cpuid_only=true, and
      vendor_cpuid_only_v2=true.
    
      - If the named CPU model has its own cache model (legacy_cache=false),
        then cache_info_cpuid4 & cache_info_amd both equal to its own cache
        model, so it uses its own cache model in 0x2 leaf regardless of the
        vendor. And AMD CPUs have all-0 leaf. Nothing will change.
    
      - For max/host/named CPU (without its own cache model), then the flag
        enable_legacy_vendor_cache is false, the legacy cache model is
        selected based on vendor.
    
        For AMD CPU, it will use legacy AMD cache but still get all-0 leaf
        due to vendor_cpuid_only=true.
    
        For non-AMD (Intel/Zhaoxin) CPU, it will use legacy Intel cache as
        expected.
    
        Here, selecting the legacy cache model based on the vendor does not
        change the previous (before the change)  behavior.
    
    Therefore, the above analysis proves that, with the help of the flag
    enable_legacy_vendor_cache, it is acceptable to select the default
    legacy cache model based on the vendor.
    
    For the CPUID 0x2 leaf, in X86CPUState, a unified cache_info is enough.
    It only needs to be initialized and configured with the corresponding
    legacy cache model based on the vendor.
    
    Intel-SIG: commit 2f5b76bce5c9 i386/cpu: Select legacy cache model based on vendor in CPUID 0x2.
    CPU new topology backporting
    
    Reviewed-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Tested-by: Yi Lai <yi1.lai@intel.com>
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Link: https://lore.kernel.org/r/20250711102143.1622339-14-zhao1.liu@intel.com
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>

--- target/i386/cpu.h
+++ target/i386/cpu.h
@@ -1916,6 +1916,7 @@ typedef struct CPUArchState {
      */
     CPUCaches cache_info_cpuid4, cache_info_amd;
     bool enable_legacy_cpuid2_cache;
+    bool enable_legacy_vendor_cache;
 
     /* MTRRs */
     uint64_t mtrr_fixed[11];
